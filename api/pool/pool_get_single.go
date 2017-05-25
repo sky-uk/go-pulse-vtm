dd460751f40aa924b46faf7aa211776477c3e4d3
rle27 <rui.leitemayermoreira@sky.uk>
2017-05-25 16:26:25 +0100

    - Finished method to get single resource - More complete model - Example for getting a single resource

--- api/pool/pool_get_single.go
+++ api/pool/pool_get_single.go
@@ -1,1 +1,23 @@
 package pool
+
+import ("net/http"
+	"github.com/sky-uk/go-brocade-vtm/api"
+
+)
+
+
+type GetSinglePool struct {
+	*api.BaseAPI
+}
+
+func NewGetSingle(poolName string) *GetSinglePool {
+	this := new(GetSinglePool)
+	this.BaseAPI = api.NewBaseAPI(http.MethodGet, "/api/tm/3.8/config/active/pools/"+poolName, nil, new(Pool))
+	return this
+
+}
+
+// GetResponse returns ResponseObject of GetSinglePool
+func (gsp GetSinglePool) GetResponse() *Pool {
+	return gsp.ResponseObject().(*Pool)
+}
