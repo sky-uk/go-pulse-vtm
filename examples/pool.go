b54a18cc17296818722a0f7063a8a21c4305e266
rle27 <rui.leitemayermoreira@sky.uk>
2017-05-25 14:01:13 +0100

    new example for pool

---
+++ examples/pool.go
@@ -0,0 +1,11 @@
+package examples
+
+import (
+	"fmt"
+	"github.com/sky-uk/go-brocade-vtm"
+	"github.com/sky-uk/go-brocade-vtm/api/pool"
+)
+
+func RunPoolExample(vtmAddress, vtmUser, vtmPassword string, debug bool) {
+
+}
