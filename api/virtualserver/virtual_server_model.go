4d82ea26fc8f2d42b22f035f1dcb6a36c004354b
Marco Masetti <marco.masetti@sky.uk>
2017-05-30 17:54:38 +0100

    virtual server model updated

--- api/virtualserver/virtual_server_model.go
+++ api/virtualserver/virtual_server_model.go
@@ -12,17 +12,27 @@ type Properties struct {
 
 // Basic : Basic virtual server configration
 type Basic struct {
-	Enabled            bool   `json:"enabled"`
-	DefaultTrafficPool string `json:"pool"`
-	Port               int    `json:"port"`
-	Protocol           string `json:"protocol"`
+	Enabled             bool        `json:"enabled"`
+	DefaultTrafficPool  string      `json:"pool"`
+	Port                int         `json:"port"`
+	Protocol            string      `json:"protocol"`
+	// all following attributes have default values set at
+    // creation time if no value is specified...
+    AddClusterIp        bool        `json:"add_cluster_ip"`         // true
+    AddXForwardedFor    bool        `json:"add_x_forwarded_for"`    // false
+    AddXForwardedProto  bool        `json:"add_x_forwarded_proto"`  // false
+    BandwidthClass      string      `json:"bandwidth_class"`        // ""
+    CloseWithRst        bool        `json:"close_with_rst"`         // false
+    CompletionRules     []string    `json:"completionrules"`        // []
+    ConnectTimeout      int         `json:"connect_timeout"`        // 10
+	FtpForceServerSecure bool       `json:"ftp_force_server_secure"` // true
+
 	// all these attributes are not mandatory at creation time...
 	/*
 	   ListenOnTrafficIps      []string        `json:"listen_on_traffic_ips"`
 	   Note                    string          `json:"note"`
 	   RequestRules            []string        `json:"request_rules"`
 	   ResponseRules           []string        `"response_rules"`
-	   CompletionRules         []string        `json:"completionrules"`
 	   FtpForceServerSecure    bool            `json:"ftp_force_server_secure"`
 	   // SslClientCertHeaders : enum can accept these strings("all", "none"
 	   // (default), "simple"
