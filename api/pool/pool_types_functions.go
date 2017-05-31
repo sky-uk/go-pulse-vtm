0951520048b5364ebffe2f4a195cdcce6bca73c6
rle27 <rui.leitemayermoreira@sky.uk>
2017-05-31 11:35:50 +0100

    fixed comments as per fmt suggestions

--- api/pool/pool_types_functions.go
+++ api/pool/pool_types_functions.go
@@ -1,6 +1,7 @@
 package pool
 
 
+// NewMemberNode - Returns the Membernodes object
 func NewMemberNode(host string, priority int, state string, weight int) MemberNode {
 	memberNode := MemberNode{
 		Node:     host,
