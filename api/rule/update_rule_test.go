9989d9445a92cf12abadefc80affb88a1053704a
Paul Warwick <paul.warwick@sky.uk>
2017-07-18 14:41:00 +0100

    Added update functionality to rule

---
+++ api/rule/update_rule_test.go
@@ -0,0 +1,28 @@
+package rule
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+var updateRuleAPI *UpdateRuleAPI
+var updateRuleName = "testRule"
+
+func updateSetup() {
+	trafficScript := []byte(`
+	if( string.ipmaskmatch( request.getremoteip(), "10.12.13.10" ) ){
+		connection.discard();
+	}`)
+	updateRuleAPI = NewUpdate(updateRuleName, trafficScript)
+}
+
+func TestUpdateMethod(t *testing.T) {
+	updateSetup()
+	assert.Equal(t, http.MethodPut, updateRuleAPI.Method())
+}
+
+func TestUpdateEndpoint(t *testing.T) {
+	updateSetup()
+	assert.Equal(t, "/api/tm/3.8/config/active/rules/"+updateRuleName, updateRuleAPI.Endpoint())
+}
