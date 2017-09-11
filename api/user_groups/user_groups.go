cd42f84fd2272288262f40881ffc3a2ff02fb827
Craig Duffin <craig.duffin@sky.uk>
2017-09-11 15:58:02 +0100

    modified to use new rest api

--- api/user_groups/user_groups.go
+++ api/user_groups/user_groups.go
@@ -1,28 +1,29 @@
 package usergroups
 
 import (
-	"github.com/sky-uk/go-brocade-vtm/api"
+	"github.com/sky-uk/go-rest-api"
 	"net/http"
+	"github.com/sky-uk/go-brocade-vtm/api"
 )
 
 const userGroupsEndpoint = "/api/tm/3.8/config/active/user_groups/"
 
-func NewGet(groupName string) *api.BaseAPI {
-	getUserGroupAPI := api.NewBaseAPI(http.MethodGet, userGroupsEndpoint+groupName, nil, new(UserGroup))
+func NewGet(groupName string) *rest.BaseAPI  {
+	getUserGroupAPI := rest.NewBaseAPI(http.MethodGet, userGroupsEndpoint+groupName, nil, new(UserGroup), new(api.VTMError))
 	return getUserGroupAPI
 }
 
-func NewGetAll() *api.BaseAPI {
-	getAllUserGroupAPI := api.NewBaseAPI(http.MethodGet, userGroupsEndpoint, nil, new(UserGroupsList))
+func NewGetAll() *rest.BaseAPI  {
+	getAllUserGroupAPI := rest.NewBaseAPI(http.MethodGet, userGroupsEndpoint, nil, new(UserGroupsList), new(api.VTMError))
 	return getAllUserGroupAPI
 }
 
-func NewPut(groupName string, userGroup UserGroup) *api.BaseAPI {
-	getAllUserGroupAPI := api.NewBaseAPI(http.MethodGet, userGroupsEndpoint+groupName, userGroup, new(UserGroup))
+func NewPut(groupName string, userGroup UserGroup) *rest.BaseAPI {
+	getAllUserGroupAPI := rest.NewBaseAPI(http.MethodGet, userGroupsEndpoint+groupName, userGroup, new(UserGroup), new(api.VTMError))
 	return getAllUserGroupAPI
 }
 
-func NewDelete(groupName string) *api.BaseAPI {
-	getAllUserGroupAPI := api.NewBaseAPI(http.MethodGet, userGroupsEndpoint+groupName, nil, nil)
+func NewDelete(groupName string) *rest.BaseAPI  {
+	getAllUserGroupAPI := rest.NewBaseAPI(http.MethodGet, userGroupsEndpoint+groupName, nil, nil, new(api.VTMError))
 	return getAllUserGroupAPI
 }
