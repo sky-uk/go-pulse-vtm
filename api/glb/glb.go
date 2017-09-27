6cbfbc0fe2662049bb4296a07cfd1e23242e1340
ppwarwick <paul.warwick88@gmail.com>
2017-09-27 10:07:51 +0100

    Implemented GLB Service Resource (#48)

---
+++ api/glb/glb.go
@@ -0,0 +1,39 @@
+package glb
+
+import (
+	"github.com/sky-uk/go-brocade-vtm/api"
+	"github.com/sky-uk/go-rest-api"
+	"net/http"
+)
+
+const glbEndpoint = "/api/tm/3.8/config/active/glb_services/"
+
+// NewCreate : used to create a new glb
+func NewCreate(glbName string, glb GLB) *rest.BaseAPI {
+	createGLBAPI := rest.NewBaseAPI(http.MethodPut, glbEndpoint+glbName, glb, new(GLB), new(api.VTMError))
+	return createGLBAPI
+}
+
+// NewGetAll : used to retrieve a list of GLBs and their HRef
+func NewGetAll() *rest.BaseAPI {
+	getAllGLBAPI := rest.NewBaseAPI(http.MethodGet, glbEndpoint, nil, new(GLB), new(api.VTMError))
+	return getAllGLBAPI
+}
+
+// NewGet - used to retrieve a GLB
+func NewGet(glbName string) *rest.BaseAPI {
+	getGLBAPI := rest.NewBaseAPI(http.MethodGet, glbEndpoint+glbName, nil, new(GLB), new(api.VTMError))
+	return getGLBAPI
+}
+
+// NewUpdate : used to update a GLB
+func NewUpdate(glbName string, glb GLB) *rest.BaseAPI {
+	updateGLBAPI := rest.NewBaseAPI(http.MethodPut, glbEndpoint+glbName, glb, new(GLB), new(api.VTMError))
+	return updateGLBAPI
+}
+
+// NewDelete : used to delete a GLB
+func NewDelete(glbName string) *rest.BaseAPI {
+	deleteGLBAPI := rest.NewBaseAPI(http.MethodDelete, glbEndpoint+glbName, nil, nil, new(api.VTMError))
+	return deleteGLBAPI
+}
---
+++ api/glb/glb_model.go
@@ -0,0 +1,54 @@
+package glb
+
+// GLB : data structure
+type GLB struct {
+	Properties Properties `json:"properties"`
+}
+
+// Properties : Properties contains the overall location configuration
+type Properties struct {
+	Basic Basic `json:"basic,omitempty"`
+	Log   Log   `json:"log,omitempty"`
+}
+
+// Basic : Basic location
+type Basic struct {
+	Algorithm            string            `json:"algorithm,omitempty"`
+	AllMonitorsNeeded    bool              `json:"all_monitors_needed"`
+	AutoRecovery         bool              `json:"autorecovery"`
+	ChainedAutoFailback  bool              `json:"chained_auto_failback"`
+	ChainedLocationOrder []string          `json:"chained_location_order,omitempty"`
+	DisableOnFailure     bool              `json:"disable_on_failure"`
+	DNSSecKeys           []DNSSecKey       `json:"dnssec_keys,omitempty"`
+	Domains              []string          `json:"domains,omitempty"`
+	Enabled              bool              `json:"enabled"`
+	GeoEffect            uint              `json:"geo_effect"`
+	LastResortResponse   []string          `json:"last_resort_response,omitempty"`
+	LocationDraining     []string          `json:"location_draining,omitempty"`
+	LocationSettings     []LocationSetting `json:"location_settings,omitempty"`
+	PeerHealthTimeout    uint              `json:"peer_health_timeout"`
+	ReturnIPSOnFail      bool              `json:"return_ips_on_fail"`
+	Rules                []string          `json:"rules,omitempty"`
+	TTL                  int               `json:"ttl"`
+}
+
+// DNSSecKey : DNS Sec key struct
+type DNSSecKey struct {
+	Domain  string   `json:"domain,omitempty"`
+	SSLKeys []string `json:"ssl_key,omitempty"`
+}
+
+// LocationSetting : settings for a location
+type LocationSetting struct {
+	Location string   `json:"location,omitempty"`
+	Weight   uint     `json:"weight"`
+	IPS      []string `json:"ips,omitempty"`
+	Monitors []string `json:"monitors,omitempty"`
+}
+
+// Log : log configuration for a GLB
+type Log struct {
+	Enabled  bool   `json:"enabled"`
+	Filename string `json:"filename,omitempty"`
+	Format   string `json:"format,omitempty"`
+}
